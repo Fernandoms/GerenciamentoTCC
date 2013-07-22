127e9c197537a5f4dae9946b8ae8af3a56a263bb
Fernando Marques de Souza Filho <fernando.marques@ice.ufjf.br>
2013-07-22 18:10:25 -0300

    Envio de DAO genérico e classe teste

--- src/com/ufjf/InterfaceDAO/ICursoDAO.java
+++ src/com/ufjf/InterfaceDAO/ICursoDAO.java
@@ -1,27 +1,7 @@
 package com.ufjf.InterfaceDAO;
 
-import java.util.List;
 
-import org.hibernate.HibernateException;
-
-import com.ufjf.DTO.Curso;
-
-public interface ICursoDAO {
-
-	boolean salvaCurso(Curso curso) throws HibernateException;
-
-	boolean editaCurso(Curso curso) throws HibernateException;
-
-	/**
-	 * Retorna uma lista de cursos com quantidade de dados informadas entre o
-	 * Ínicio e o Fim informados. Caso Parametros sejam iguais a null, retorna
-	 * todos os Cursos do banco.
-	 * 
-	 * @param inicio
-	 * @param fim
-	 * @return {@link List}<{@link Curso}>
-	 * @throws HibernateException
-	 */
-	List<Curso> retornaCursos(int inicio, int fim) throws HibernateException;
-
+public interface ICursoDAO extends IGenericoDAO {
+	
+	
 }
